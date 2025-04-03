017473f93d1e752b7a1b5862fe3420e80d77e99c
SByJoao <joaoprogit@gmail.com>
2025-04-03 11:56:04 -0300

    dark mode

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -5,5 +5,27 @@ $(document).ready(function(){
     })
 })
 
+// dark mode // 
 
+let darkMode = localStorage.getItem('darkmode')
+const themeSwitch = document.getElementById('switch-theme')
+
+const enableDarkmode = () => {
+    document.body.classList.add('darkmode')
+    localStorage.setItem('darkmode', 'active')
+}
+
+const disableDarkmode = () => {
+    document.body.classList.remove('darkmode')
+    localStorage.setItem('darkmode', null)
+}
+
+if(darkMode === "active") enableDarkmode()
+
+themeSwitch.addEventListener("click", () => {
+    darkMode = localStorage.getItem('darkmode')
+darkMode !== "active" ? enableDarkmode () : disableDarkmode ()
+})
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------//
 
